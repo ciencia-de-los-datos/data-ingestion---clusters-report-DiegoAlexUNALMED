06936cf4c52f03f40568db3a7cb33d3a5c6c298d
DiegoAlexUNALMED <112149672+DiegoAlexUNALMED@users.noreply.github.com>
2022-10-09 17:40:03 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,60 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    with open('clusters_report.txt',mode = 'r') as archivo:
+        #Lectura de texto
+        texto = archivo.readlines()
 
+        #Extracción de campos
+        campos1 = texto[0].strip()
+        campos2 = texto[1].strip()
+        campos = [campos1[0:7],campos1[9:21]+campos2[0:14],campos1[25:39]+campos2[16:30],campos1[41:67]]
+        campos = list(map(lambda x: x.lower().replace(" ","_"),campos))
+
+        #Procesar la entrada de la información a data estructurada.
+
+        entrada = list(map(lambda x: x.strip(),texto[4:]))
+        entrada = [x for x in entrada if x != ""]
+
+        entrada_procesada = []
+
+        for i in range(13):
+            j = i + 1
+            fila = []
+            if j in (1, 4, 5, 6, 7, 8, 9, 10, 13):
+                if j < 10:
+                    for k in range(len(entrada)):
+
+                        if entrada[k][0] == str(j):
+                            fila = [entrada[k][0], entrada[k][5:9], entrada[k][22:26], entrada[k][38:]+" "+entrada[k+1]+" "+entrada[k+2]+" "+ entrada[k+3]]
+                            break
+                else:
+                    for k in range(len(entrada)):
+
+                        if entrada[k][0:2] == str(j):
+                            fila = [entrada[k][0:2], entrada[k][5:9], entrada[k][22:26], entrada[k][38:]+" "+entrada[k+1]+" "+entrada[k+2]+" "+ entrada[k+3]]
+                            break
+            elif j in (2,12):
+                if j == 2:
+                    for k in range(len(entrada)):
+                        if entrada[k][0] == str(j):
+                            fila = [entrada[k][0], entrada[k][5:9], entrada[k][22:26], entrada[k][38:]+" "+entrada[k+1]+" "+entrada[k+2]+" "+ entrada[k+3]+" "+ entrada[k+4]]
+                            break
+                else:
+                    for k in range(len(entrada)):
+                        if entrada[k][0:2] == str(j):
+                            fila = [entrada[k][0:2], entrada[k][5:9], entrada[k][22:26], entrada[k][38:]+" "+entrada[k+1]+" "+entrada[k+2]+" "+ entrada[k+3]+" "+ entrada[k+4]]
+                            break
+            elif j == 3:
+                for k in range(len(entrada)):
+                    if entrada[k][0] == str(j):
+                        fila = [entrada[k][0], entrada[k][5:9], entrada[k][22:26], entrada[k][38:]+" "+entrada[k+1]+" "+entrada[k+2]]
+                        break
+            elif j == 11:
+                for k in range(len(entrada)):
+                    if entrada[k][0:2] == str(j):
+                        fila = [entrada[k][0:2], entrada[k][5:9], entrada[k][22:26], entrada[k][38:]+" "+entrada[k+1]]
+                        break
+            entrada_procesada.append(fila)
+    df = pd.DataFrame(entrada_procesada, columns = campos)
     return df
